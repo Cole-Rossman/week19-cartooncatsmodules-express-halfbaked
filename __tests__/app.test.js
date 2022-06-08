91ce528f31d03c01c0265387660fb055acf36bdd
Cole Rossman <colerossman99@gmail.com>
2022-06-08 13:03:26 -0700

    Cats class getAll function complete and passes test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,7 +3,7 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const { cats } = require('../data/cats');
+const Cat = require('../lib/models/Cats');
 
 describe('cats routes', () => {
   beforeEach(() => {
@@ -12,6 +12,7 @@ describe('cats routes', () => {
 
   it('/cats should return a list of cats', async () => {
     const res = await request(app).get('/cats');
+    const cats = await Cat.getAll();
     const expected = cats.map((cat) => {
       return { id: cat.id, name: cat.name };
     });
